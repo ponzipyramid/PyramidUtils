3efb410034b99d4e9d81a6edfb363569d3492a36
ponzipyramid <ponzipyramid123@proton.me>
2024-07-18 20:34:37 -0400

    moved patch out

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -1,82 +1,9 @@
 #include "Hooks.h"
 #include "ActorManager.h"
-#include "Trampoline.h"
 #include "MarkerManager.h"
+#include "Patch.h"
 
 using namespace PyramidUtils;
-namespace Patches
-{
-	constexpr REL::RelocationID AddMarkerId{ 50851, 51728 };
-	inline REL::Relocation<bool (*)(const void*, void*,
-		RE::NiPoint3*, const RE::RefHandle&, std::int32_t)>
-		AddMarker{ AddMarkerId };
-
-	inline uint64_t test;
-	void UpdateQuests(void* a_1, void* a_2, RE::NiPoint3* a_pos, const RE::RefHandle& a_refHandle, std::uint32_t a_markerGotoFrame)
-	{
-		const auto marker = RE::TESObjectREFR::LookupByHandle(a_refHandle).get();
-		MarkerManager::SetMarker(marker);
-	}
-
-	std::uintptr_t getJmpTarget(std::uintptr_t a_addr) {
-		uint8_t opcode = *(uint8_t*)a_addr;
-
-		if (opcode != 0xE9)
-			return (uintptr_t)-1;
-
-		int32_t offset = *(int32_t*)(a_addr + 1);
-
-		uintptr_t target = a_addr + 5 + offset;
-
-		return target;
-	}
-
-	struct UpdateQuestsHook : hooks::Hook<5>
-	{
-		struct HookCodeGenerator : Xbyak::CodeGenerator
-		{
-			HookCodeGenerator(std::uintptr_t a_cnoAddr)
-			{
-				Xbyak::Label hookLabel;
-				Xbyak::Label cnoLabel;
-
-				push(rcx);
-				push(rdx);
-				push(r8);
-				push(r9);
-
-				call(ptr[rip + hookLabel]);
-
-				pop(r9);
-				pop(r8);
-				pop(rdx);
-				pop(rcx);
-
-				jmp(ptr[rip + cnoLabel]);
-
-				L(hookLabel), dq(reinterpret_cast<std::uintptr_t>(&UpdateQuests));
-				L(cnoLabel), dq(a_cnoAddr);
-
-				ready();
-			}
-		};
-
-		UpdateQuestsHook(std::uintptr_t a_hookedAddress, uintptr_t a_cnoAddr) :
-			Hook{ a_hookedAddress, HookCodeGenerator{ a_cnoAddr } }
-		{}
-	};
-
-	void Install() {
-		const auto address = REL::RelocationID{ 50826, 51691 }.address() + REL::VariantOffset{ 0x114, 0x180, 0x114 }.offset();
-
-		const auto target = getJmpTarget(address);
-
-		UpdateQuestsHook updateQuestsHook{ address, target };
-
-		static hooks::DefaultTrampoline tramp1{ updateQuestsHook.getSize() };
-		tramp1.write_branch(updateQuestsHook);
-	}
-}
 
 void Hooks::Install() {
 	logger::info("installing hooks");
@@ -99,7 +26,7 @@ void Hooks::Install() {
 	HINSTANCE dllHandle = LoadLibrary(TEXT("CompassNavigationOverhaul.dll"));
 	if (dllHandle != NULL) {
 		logger::info("installing cno patch");
-		Patches::Install();
+		Patch::Install();
 	} else {
 		logger::info("installing cno hook");
 		REL::Relocation<std::uintptr_t> udq{ REL::RelocationID{ 50826, 51691 }, REL::VariantOffset{ 0x114, 0x180, 0x114 } };
